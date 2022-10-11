864f492e5132d67cdf0bd0d085a93d18bd890f55
ranawp <ranasaha4224#gmail.com>
2022-10-11 22:00:00 +0600

    dark mode added with localstorage

--- js/index.js
+++ js/index.js
@@ -27,3 +27,21 @@ const toggleMenu = () => {
 menuToggleIcon.addEventListener('click', toggleMenu)
 
 
+// Switch theme/add to local storage 
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+
+const currentTheme = localStorage.getItem('currentTheme');
+
+if (currentTheme) {
+    bodyElement.classList.add('light-theme');
+}
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('light-theme');
+
+    if (bodyElement.classList.contains('light-theme')) {
+        localStorage.setItem('currentTheme', 'themeActivate');
+    } else {
+        localStorage.removeItem('currentTheme');
+    }
+})
